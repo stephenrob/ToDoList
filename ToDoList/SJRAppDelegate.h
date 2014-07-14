9605a76af59cf7f3048156ab542d7b9b9cbbf078
Stephen Robinson <stephen@stephen-robinson.co.uk>
2014-07-14 11:03:57 +0100

    Initial Commit

---
+++ ToDoList/SJRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SJRAppDelegate.h
+//  ToDoList
+//
+//  Created by Stephen Robinson on 14/07/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SJRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Stephen Robinson on 14/07/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SJRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SJRAppDelegate class]));
+    }
+}
